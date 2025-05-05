1fa469f0bd7328ceb35c304cc167c4b2b7222e9a
Luxter77 <19197331+Luxter77@users.noreply.github.com>
2025-05-04 23:39:08 -0300

    it'd be nicer if this wasnt burried into the readme and not documented anyweere else

--- nn_x/embedding.py
+++ nn_x/embedding.py
@@ -8,8 +8,8 @@ NOMIC_WORD_TO_TOKEN_RATIO = 1.52
 # there are on average 0.2591943988246274 tokens per char in a window
 NOMIC_CHAR_TO_TOKEN_RATIO = 0.26
 
-nomic_embedding_tokenizer = AutoTokenizer.from_pretrained('bert-base-uncased')
-nomic_embedding_model     = AutoModel.from_pretrained('nomic-ai/nomic-embed-text-v1.5', trust_remote_code=True, safe_serialization=True)
+nomic_embedding_tokenizer = AutoTokenizer.from_pretrained('bert-base-uncased', trust_remote_code=True, max_length=NOMIC_EMBEDDING_MAX_WINDOW_SIZE)
+nomic_embedding_model     = AutoModel.from_pretrained('nomic-ai/nomic-embed-text-v1.5', trust_remote_code=True, rotary_scaling_factor=2)
 nomic_embedding_model.eval()
 
 nomic_embedding_model.to('cuda' if torch.cuda.is_available() else 'cpu')
